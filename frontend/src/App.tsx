a73cd77b3cd74f409391e7f0e917474a4f8d3518
Derrick Hammer <derrick@derrickhammer.com>
2022-04-10 22:54:11 -0400

    *Implement new UX and change how install flow is controlled so UI only reads state, but does not set it to prevent bugs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,7 @@
 import {useEffect, useState} from "react";
+// @ts-ignore
+import IconSetting from "./assets/images/icon-setting.png";
+import "./assets/css/style.css";
 
 export default function App() {
     const [status, setStatus] = useState();
@@ -6,20 +9,38 @@ export default function App() {
 
     window.runtime.EventsOn("status", setStatus)
     window.runtime.EventsOn("setInstallState", setInstallState)
-
-    useEffect(() => {
-        if (1 === installState) {
-            window.runtime.EventsEmit("install");
-        }
-    });
-
     return (
-        <>
+        <div className="content-container">
+            <Icon installState={installState} />
+            <Header installState={installState}/>
             <Instructions installState={installState}/>
-            <InstallBtn installState={installState}  setInstallState={setInstallState}/>
-            <p>{status}</p>
-        </>
+            <InstallBtn installState={installState} setInstallState={setInstallState}/>
+            {installState > 0 && <p>{status}</p>}
+        </div>
     )
+}
+
+function Icon({installState}) {
+    let classes = ['header-icon'];
+    if (1 === installState) {
+        classes.push("rotating");
+    }
+
+    return <img className={classes.join(" ")} src={IconSetting}/>
+}
+
+function Header({installState}) {
+    let message = "Lume Web Extension Installer";
+
+    if (1 == installState) {
+        message = "Installing Extension";
+    }
+
+    if (2 == installState) {
+        message = "Extension installed successfully";
+    }
+
+    return <h1>{message}</h1>;
 }
 
 
@@ -29,17 +50,32 @@ function Instructions({installState}) {
     }
 
     return (
-        <div>
-            <p>Click "install" to load the Lume Web extension into Chrome and Brave</p>
-            <p style={{color:"red"}}>Please <b>ensure</b> your Chrome and/or Brave browser is completely closed before proceeding!</p>
-        </div>
+        <>
+            <div className="paragraph">
+                <p>Click install to load the Lume Web Extension into Chrome and Brave.</p>
+            </div>
+            <div className="error-message">
+
+                <div className="error-message-content">
+                    <span>Please ensure your Chrome and/or Brave browser is <strong>completely closed</strong> before proceeding.</span>
+                </div>
+            </div>
+        </>
     );
 }
 
-function InstallBtn({installState,setInstallState}) {
+function InstallBtn({installState, setInstallState}) {
     if (0 < installState) {
         return
     }
 
-    return <button onClick={() => setInstallState(1)}>Install</button>;
+    const startInstall = () => {
+        window.runtime.EventsEmit("install");
+    };
+
+    return (
+        <div className="action-buttons">
+            <button className="btn" onClick={() => startInstall()}>Install extension</button>
+        </div>
+    );
 }
